b0f8d1ee1fae690c8c9dd1566162120195acd2a8
Anıl Karacan <aanilkaracann@gmail.com>
2025-10-10 17:12:10 +0300

    Initial Commit

---
+++ SkinRoutineTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SkinRoutineTracker
+//
+//  Created by Anıl Karacan on 10.10.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SkinRoutineTracker/SkinRoutineTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkinRoutineTrackerApp.swift
+//  SkinRoutineTracker
+//
+//  Created by Anıl Karacan on 10.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct SkinRoutineTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
